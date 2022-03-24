d09601befd8b6f292f06e9ee27c7fb54e4477082
smilesformiles3 <nikhil0103@outlook.com>
2022-03-23 21:30:39 -0400

    First version!

--- main.py
+++ main.py
@@ -1,45 +1,57 @@
 import mido
-import numpy
-import ffmpy
-
-
-def tempo_change(_input, scale, output):
-    ff = ffmpy.FFmpeg(inputs={_input: None}, outputs={output: ["-filter:a", "atempo=" + str(scale)]})
-    ff.run()
-
-
-def pitch_change(_input, origrate, scale, output):
-    ff = ffmpy.FFmpeg(inputs={_input: None},
-                      outputs={output: ["-af", "asetrate=" + str(origrate) + "*" + str(scale) + ",atempo=" + str(
-                          1 / scale) + ",aresample=" + str(origrate)]})
-    ff.run()
+import numpy as np
+from scipy.io import wavfile as wav
+from scipy import fftpack
+import pyrubberband as pyrb
+from math import log2
 
 
 class Track:
-    def __init__(self, instrument, length, rate=48000):  # length in seconds, rate in samples/sec
-        self.instrument = instrument
-        self.msgs = []
-        self.waveform = numpy.zeros(length * rate)
+    def __init__(self, instrument, midi, track, rate=48000):  # length in seconds, rate in samples/sec
+        # the tempo of the test song, wii sports, is 504201 and tpb = 960
+        self.instrument = instrument  # meant to be waveform of instrument
+        self.msgs = midi.tracks[track]  # midi messages
+        self.rate = rate
+        self.pitch = self.instrument_base_pitch()
+        self.midi = midi
+        self.tempo = [i for i in midi.tracks[0] if i.type == 'set_tempo'][0].tempo
+        self.ticks_per_beat = midi.ticks_per_beat
+        self.length = ([i for i in midi.tracks[0] if i.type == 'end_of_track'][0].time * self.tempo)/(self.ticks_per_beat * 1000000)
+        self.waveform = np.zeros(self.length * rate)  # output waveform
 
-    def midi_to_waveform(self, msgs):
+    def midi_to_waveform(self):
         pos = 0  # in ticks
-        notes = []
-        for msg in msgs:
-            if msg.time == 0:
-                notes.append(msg.note)
-            else:
-                for note in notes:
-                    # modify instrument clip with pitch and tempo, pad with zeros
-                    clip = numpy.zeros(pos)
-                    clip += self.instrument  # todo: tune the instrument
-                    clip += numpy.zeros(self.waveform.length - len(clip))
-                    self.waveform = numpy.add(self.waveform, clip)
-                pos += msg.time
+        notes = dict()
+        for msg in self.msgs:
+            if not msg.type == 'note_on':
+                continue
+            notes.setdefault(msg.note, pos)  # structure: key = pitch, value = start position
+            pos += msg.time
+            if msg.velocity == 0 or msg.type == 'note_off':  # note has ended - add it to the waveform!
+                startpos = notes.pop(msg.note)
+                # modify instrument clip with pitch and tempo, pad with zeros (convert ticks to samples)
+                preclip = np.zeros(int(startpos * (self.tempo * self.rate / (self.ticks_per_beat * 1000000))))
+                instrclip = pyrb.pitch_shift(pyrb.time_stretch(self.instrument, self.rate, len(self.instrument)/((pos - startpos) * (self.tempo * self.rate / (self.ticks_per_beat * 1000000)))), self.rate, -12*log2(self.pitch/(440*(2**((msg.note-69)/12)))))
+                postclip = np.zeros(len(self.waveform) - len(preclip) - len(instrclip))
+                clip = np.concatenate((preclip, instrclip, postclip))
+                self.waveform += clip
+
+    def instrument_base_pitch(self):
+        out = fftpack.rfft(self.instrument)  # reverse fast fourier transform, to convert wavelengths into time
+        power = np.abs(out) ** 2  # power array
+        freqs = fftpack.fftfreq(out.size, d=1 / self.rate)
+        return freqs[np.argmax(power)]
+
+    def export(self, filename):
+        wav.write(filename + ".wav", self.rate, self.waveform)
 
 
-mid = mido.MidiFile('wii-wiisports-titlescreen.midi')
-
-for i, track in enumerate(mid.tracks):
-    print('Track {}: {}'.format(i, track.name))
-print(mid.ticks_per_beat)
-print(mid.tracks[2])
+# mid = mido.MidiFile('wii-wiisports-titlescreen.midi')
+# print(mid.tracks[0])
+# rate, data = wav.read('converted.wav')
+#
+# print(mid.tracks[2])
+#
+# # test = Track(data.T[0,:], mid.tracks[2], 504201, 960, 125)
+# # test.midi_to_waveform()
+# # test.export()
